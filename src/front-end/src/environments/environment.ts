e2922338b95aade996c61ccbcfdc4b89f0641350
Tuan Nguyen <105630493+tuannq1@users.noreply.github.com>
2025-10-19 14:45:58 +0700

    update environment variable

--- src/front-end/src/environments/environment.ts
+++ src/front-end/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://glorious-space-potato-q5w647gv49r3x5w9-5141.app.github.dev',
+  apiBaseUrl: 'http://localhost:5141',
 //   apiBaseUrl: 'https://glorious-space-potato-q5w647gv49r3x5w9-5141.app.github.dev'
 };
